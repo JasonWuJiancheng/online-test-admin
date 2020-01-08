3c2ab2505a1ccbe972ec8d8922104f42d110cef6
JasonWu <jasonwu_edu@qq.com>
2020-01-08 21:07:45 +0800

    完成examtype页面表格加载数据功能

--- src/com/onlinetest/admin/Test.java
+++ src/com/onlinetest/admin/Test.java
@@ -4,6 +4,9 @@ import com.onlinetest.admin.entity.ExamType;
 import com.onlinetest.admin.service.ExamTypeService;
 import com.onlinetest.admin.service.impl.ExamTypeServiceImpl;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author JasonWu
  * @create 2020-01-06-21:55
@@ -12,14 +15,20 @@ public class Test {
 
     public static void main(String[] args) {
 
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime start = null;
+        LocalDateTime end = null;
+        String time1 = null;
+        String time2 = null;
         ExamTypeService examTypeService = new ExamTypeServiceImpl();
+        for(int i=0;i<99;i++){
+            start = LocalDateTime.now().plusHours(i*10);
+            end = LocalDateTime.now().plusDays(i);
+            time1 = dateTimeFormatter.format(start);
+            time2 = dateTimeFormatter.format(end);
+            ExamType examType = new ExamType(i+"","期末考试"+i,time1,time2);
+            examTypeService.insertExamType(examType);
+        }
 
-        ExamType examType = new ExamType("1","2018-2019学年上半学期期末考试","20190511","20191116");
-//        examTypeService.insertExamType(examType);
-
-//        List<ExamType> allExamTypes = examTypeService.getAllExamType();
-//        System.out.println(allExamTypes);
-
-//        examTypeService.updateExamType(examType);
     }
 }
